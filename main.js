1028c4735f712209f26845c587dddef1ecbbb942
sho-webcoding <141204357+sho-webcoding@users.noreply.github.com>
2024-03-20 17:05:25 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+$(function () {
+    setTimeout(function () {
+        $('body').fadeIn(1500);
+    }, 600);
+});
+
+$(window).scroll(function () {
+    $('.none').each(function () {
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100 && $(this).hasClass('none')) {
+            $(this).removeAttr('class');
+            $(this).attr('class', 'slide');
+        }
+    });
+    $('.thumb button').each(function () {
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+        if ($(window).width() > 768 && scroll > elemPos - windowHeight + 100 && $(this).hasClass('white')) {
+            $(this).removeAttr('class');
+            $(this).attr('class', 'black');
+        }
+        if ($(window).width() < 768 && scroll > elemPos - windowHeight + 100 && $(this).hasClass('white')) {
+            setTimeout(function () {
+                $(this).removeAttr('class');
+                $(this).attr('class', 'black');
+            }, 200);
+        }
+    });
+});
